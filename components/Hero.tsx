85024829d5d0512088857abef189f851596fdb6d
TheLastpressedkey <89164291+TheLastpressedkey@users.noreply.github.com>
2024-05-19 15:51:14 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+           Bonjour, c'est Hugues Frantz developeur, juriste et artiste basé à Lyon 
           </p>
 
           <a href="#about">
