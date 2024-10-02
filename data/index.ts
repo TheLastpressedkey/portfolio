08cd736cfda01e561957a7335797ef98727ebff6
Hugues Frantz <89164291+TheLastpressedkey@users.noreply.github.com>
2024-10-02 18:35:53 +0200

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -165,13 +165,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "/vfp-consult.com/",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://twitter.com/riichkidz",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/kvption/",
   },
 ];
