478c1f86da7336a1b437c7c0b978946162274064
Hugues Frantz <89164291+TheLastpressedkey@users.noreply.github.com>
2024-10-02 17:48:40 +0200

    Update Clients.tsx

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -9,8 +9,8 @@ const Clients = () => {
   return (
     <section id="testimonials" className="py-20">
       <h1 className="heading">
-        Kind words from
-        <span className="text-purple"> satisfied clients</span>
+        Quelques témoignages de mes 
+        <span className="text-purple">collaborateurs</span>
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10">
