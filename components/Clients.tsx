16385affdd588a14bebab2c446fd713eea747088
Hugues Frantz <89164291+TheLastpressedkey@users.noreply.github.com>
2024-10-02 17:34:48 +0200

    Update Clients.tsx

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,21 +2,21 @@
 
 import React from "react";
 
-import { companies, testimonials } from "@/data";
+import { collaborators, testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
   return (
     <section id="testimonials" className="py-20">
       <h1 className="heading">
-        Kind words from
-        <span className="text-purple"> satisfied clients</span>
+        Retours bienveillants de mes
+        <span className="text-purple"> collaborateurs et partenaires</span>
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10">
         <div
-          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
-          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
+          // Adjusting the height and responsive design to fit well with testimonials
+          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden"
         >
           <InfiniteMovingCards
             items={testimonials}
@@ -26,18 +26,18 @@ const Clients = () => {
         </div>
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
-          {companies.map((company) => (
-            <React.Fragment key={company.id}>
+          {collaborators.map((collaborator) => (
+            <React.Fragment key={collaborator.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
                 <img
-                  src={company.img}
-                  alt={company.name}
+                  src={collaborator.img}
+                  alt={collaborator.name}
                   className="md:w-10 w-5"
                 />
                 <img
-                  src={company.nameImg}
-                  alt={company.name}
-                  width={company.id === 4 || company.id === 5 ? 100 : 150}
+                  src={collaborator.nameImg}
+                  alt={collaborator.name}
+                  width={collaborator.id === 4 || collaborator.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
                 />
               </div>
