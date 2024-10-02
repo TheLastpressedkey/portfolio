d106c45f3d8421155aa6cd587883e04b03751796
Hugues Frantz <89164291+TheLastpressedkey@users.noreply.github.com>
2024-10-02 19:11:23 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transformer les concepts en expériences utilisateur fluides"
+            words="Innover avec impact : concevoir des solutions sur mesure pour les défis de demain."
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
