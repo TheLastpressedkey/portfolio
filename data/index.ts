210b9c0ca5830eb9bc8d01f52e1675d8d6ca2443
Hugues Frantz <89164291+TheLastpressedkey@users.noreply.github.com>
2024-10-02 15:41:43 +0200

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -173,31 +173,24 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Co-fondateur - Prometheus SARL",
+    desc: "Gestion de projets de digitalisation, diagnostic d'activités pour la transformation digitale, conception et développement de solutions logicielles sur mesure.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
-  {
+   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
+    title: "Chargé de Mission Reporting & Q/A - Groupe Casino",
+    desc: "Suivi informatique et analyse financière, reporting et analyse des marchés pour optimiser les performances des équipes et des systèmes.",
+    className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
   {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    id: 3,
+    title: "Assistant Juridique",
+    desc: "Réalisation de veilles stratégiques, traitement des dossiers de litiges, et assistance auprès des avocats dans la gestion des dossiers.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "/exp2.svg",
   },
 ];
 
